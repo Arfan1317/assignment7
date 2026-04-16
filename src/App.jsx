494b6c5df92c68fa51dcf11ef4d4fdfc26ab2272
Arfan1317 <piasharfanulhoque@gmail.com>
2026-04-16 09:49:11 +0600

    Setted all the folder and file paths

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import FriendDetails from "./pages/FriendDetails";
+import Timeline from "./pages/Timeline";
+import Stats from "./pages/Stats";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+  return (
+   
+    <BrowserRouter>
+      <Routes>
+        
+        
+        <Route element={<MainLayout />}>
+          
+          
+          <Route path="/" element={<Home />} />
+          
+         
+          <Route path="/friend/:id" element={<FriendDetails />} />
+          
+         
+          <Route path="/timeline" element={<Timeline />} />
+          <Route path="/stats" element={<Stats />} />
+          
+        </Route>
+
+      
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
